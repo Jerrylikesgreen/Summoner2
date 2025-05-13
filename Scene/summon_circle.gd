62816b91d1a3b21a4d9e9ce627bb032d3c29b146
Jerrylikesgreen <gerardo.azamar11@gmail.com>
2025-05-12 21:47:14 -0400

    Refactored - Created summon manager -
    Refactored some things to make it more "clean" Also created a manager for summons, it currently will detect if moving and change animation. has an area detection almost set up.

--- Scene/summon_circle.gd
+++ Scene/summon_circle.gd
@@ -1,15 +1,16 @@
 class_name SummonCircle extends Node2D
 
-@onready var world: Node2D = $".."
+@onready var target: Node2D = $".."
 @onready var icon: Sprite2D = $Icon
+
+
 enum SummonColor { YELLOW, GREEN, BLUE }
 @export var summon_color : SummonColor = SummonColor.YELLOW
-
 @export var _is_summoning: bool = false
 
-const SUMMONS = preload("res://summons.tscn")
+const SUMMONS = preload("res://Scene/summon_manager.tscn")
 var summon_target_position:Vector2
-
+var summon = SUMMONS.instantiate()
 
 
 func _ready() -> void:
@@ -18,18 +19,18 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	get_input()
-	 
 
 
 func get_input() -> void:
 	if Input.is_action_pressed("Summon"):
 		icon.set_visible(true)
-		
+		color_summon_circle() 
 		summon_target_position = get_global_mouse_position()
 		set_position(summon_target_position)
 		_is_summoning = true
 		if Input.is_action_just_pressed("SummonScrollUp"):
 			summon_color += 1
+			
 			if summon_color > 2:
 				summon_color = 0
 
@@ -53,9 +54,30 @@ func get_input() -> void:
 			spawn_summon()
 			return
 
+func color_summon_circle() -> void:
+	match summon_color:
+		SummonColor.YELLOW:
+			icon.modulate = Color.YELLOW
+		SummonColor.GREEN:
+			icon.modulate = Color.GREEN
+		SummonColor.BLUE:
+			icon.modulate = Color.BLUE
+
+
+func color_summon() -> Color:
+	match summon_color:
+		SummonColor.YELLOW:
+			return Color.YELLOW
+		SummonColor.GREEN:
+			return Color.GREEN
+		SummonColor.BLUE:
+			return Color.BLUE
+		_:
+			return Color.WHITE  # fallback if no match
 
 func spawn_summon() -> void:
-	var summon = SUMMONS.instantiate()
-	world.add_child(summon)
-	summon.position = summon_target_position
+	var spawn  = SUMMONS.instantiate() 
+	target.add_child(spawn)
+	spawn.modulate = color_summon()
+	spawn.position = summon_target_position
 	return
